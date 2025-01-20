7b40dbdd40089e9b77f7c21b6e60c2fedc14aaed
Dmitri <Dmitri@gmail.com>
2025-01-20 14:01:14 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,84 @@
+// console.log(something);
+// let box;
+// console.log(box);
+
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false,
+// }
+// console.log(obj["name"]);
+// let arr = ['plumb.png','orange.jpg','apple.bmp',6];
+// console.log(arr[3]);
+// const arr = [1,2,3];
+// arr[10] = '3456';
+// console.log(arr[10]);
+//
+//
+// const arrObj = {
+//     0: 1,
+//     1: 2,
+//     2: 3,
+//     abc: {
+//         df: [{},{}],
+//         def: {
+//
+//         }
+//     }
+// };
+//
+// const b = 'b';
+//
+// // arrObj.b = '1234';
+// arrObj['b'] = '1234';
+// console.log(arrObj.b);
+//
+// console.log(arrObj["b"]);
+// const obj = {
+//     'Anna': 500,
+//     'Alic': 800,
+// };
+
+// alert('Hello');
+
+
+// const result = confirm(
+//     "Are you here?"
+// );
+// console.log(result);
+
+// const answer = +prompt('Are you 18?', "18");
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt('What is ur name?','');
+// answers[1] = prompt('What is ur surname?','');
+// answers[2] = prompt('How old are you?','');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/`);
+//
+// const user = 'Ivan';
+// alert(`Hello, ${user}`);
+
+
+// console.log('arr' + " - object");
+// console.log(4 + " - object");
+
+
+let incr = 10,
+    decr = 10;
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+const num = true;
+const second = false;
+console.log(num ||  !second);
